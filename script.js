378d32d66b93687c4f2b09ffd96842e368383645
Ali Alamdari <alinalamdari@gmail.com>
2024-09-18 06:33:03 +0330

    first commit, add basic gpt-based html/css

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('choose-file').addEventListener('click', function () {
+    alert('Select file destination dialog would appear here.');
+});
+
+document.getElementById('start-download').addEventListener('click', function () {
+    alert('Download started!');
+});
