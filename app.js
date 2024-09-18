f5aaf5ccebb9bb25ab147d8501222872ae7ae6e0
Ali Alamdari <alinalamdari@gmail.com>
2024-09-18 17:18:03 +0330

    add child-process functionality and auto open browser

--- app.js
+++ app.js
@@ -11,13 +11,28 @@
 //      resolution: "1080"
 //  }
 
-const { spawn } = require('child_process');
-const express = require('express');
+// const { spawn } = require('child_process');
+// const express = require('express');
+
+import {spawn} from 'child_process';
+import express from 'express';
+import open from 'open';
+import path from 'path';
 
 const app = express();
-const port = 3000;
+const __filename = import.meta.filename;
+const __dirname = path.dirname(__filename);
+const port = 8086;
+
+open(`http://localhost:${port}`).then(
+    () => console.log(`I tried to open on port ${port}`),
+);
 
 app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './index.html'));
+})
 
 /**
  * POST route to start video download
@@ -25,20 +40,36 @@ app.use(express.json());
  */
 app.post('/download', (req, res) => {
     const videoUrl = req.body.url;
-    const resolution = req.body.resolution || 'best';  // Fallback to 'best' resolution if not provided
+    const filePath = req.body.file;
+    const resolution = req.body.resolution;
+    console.log('its whole req.body: ' + req.body.toString());
+    console.log(`i extracted req.body... : ${videoUrl} ${filePath} ${resolution}`);
 
-    if (!videoUrl) {
-        return res.status(400).send('URL is required');
+    if (!videoUrl || !filePath) {
+        return res.status(400).json({ok:false, message:'url and file are required'});
     }
 
-    const ytDlpArgs = ['-f', resolution, videoUrl];
+    //make up args for yt-dlp
+    //yt-dlp -f "bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]/best[ext=mp4][height<=1080]"
+    //  --merge-output-format mp4 -o "/path/to/your/directory/filename.mp4" <video_url>
+    const ytDlpArgs = ['-f',
+        `bestvideo[ext=mp4][height<=${resolution}]+bestaudio[ext=m4a]/best[ext=mp4][height<=${resolution}]`,
+        "--merge-output-format",
+        "mp4",
+        "-o",
+        filePath,   //possible error fix: `"${filePath}"`
+        videoUrl];
+    console.log("now i log the prepared Args: " + ytDlpArgs);
 
     // Spawn yt-dlp process
     const ytDlpProcess = spawn('yt-dlp', ytDlpArgs);
+    console.log('now i have spawned yt-dlp subprocess!');
 
     // Track real-time download progress
     ytDlpProcess.stderr.on('data', (data) => {
         const output = data.toString();
+        console.log("i have just logging 'data' :> " + output); //dev temp
+        return; //dev temp
         const progressData = parseYtDlpOutput(output);
 
         if (progressData) {
